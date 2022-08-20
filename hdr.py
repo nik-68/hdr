a2dda3d6b280eade64cd30e37efc67b56bc086ce
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-20 19:49:57 +0300

    Update hdr.py

--- hdr.py
+++ hdr.py
@@ -466,7 +466,7 @@ print(Fore.GREEN+"З А Г Р У З К А....")
 time.sleep(3.5)
 os.system("clear")
 
-print(Fore.BLUE+"
+print("""
      __      ANONYMOUS       _____
     / /  __ _ _   _  ___ _  |___  |
    / /  / _` | | | |/ _ \ '__| / /
@@ -474,9 +474,7 @@ print(Fore.BLUE+"
   \____/\__,_|\__, |\___|_|  /_/
               |___/
   ADDED NEW METHOD AND BYPASS 
-\n")
-
-
+""")
 def starturl(): # in questa funzione setto l'url per renderlo usabile per il futuro settaggio delle richieste HTTP.
 	global url
 	global url2
